4a024521691adbc2055671e6bd9a8840e5ce73c4
Gabriel Santos <82888506+GabrielSantosCruz@users.noreply.github.com>
2024-04-16 15:09:23 -0300

    Add features

--- ErroCaixa.v
+++ ErroCaixa.v
@@ -1,11 +1,11 @@
-module ErroCaixa(
-	input Low, Mid, High, Temp, Usolo, Uar
-	output ErroMedida, Aspersor, Gotejamento, VEntrada, Alarme
-);
+module ErroCaixa(Low, Mid, High, Temp, Usolo, Uar, ErroMedida, Aspersor, Gotejamento, VEntrada, Alarme);
+
+input Low, Mid, High, Temp, Usolo, Uar;
+output ErroMedida, Aspersor, Gotejamento, VEntrada, Alarme;
 	
 	// Sistema de erro
 	//=================================================
-	wire Low_inv, Mid_inv, High_inv, NotAB, NotBC;
+	wire Low_inv, Mid_inv, NotAB, NotBC;
 	
 	// Inversao:
 	not UI1(Low_inv, Low);
@@ -14,6 +14,26 @@ module ErroCaixa(
 	// Portas AND:
 	and UI3(NotAB, Low_inv, Mid);
 	and UI4(NotBC, Mid_inv, High);
-	or UI5(ErroMedida, NotBC, NotAB);
+	or ErroNivel(ErroMedida, NotBC, NotAB);
+	// SAIDA: LED 9
 	//=================================================
-endmodule
+	
+	// Sistema de Aspersao
+	//=================================================
+	wire Erro_inv;
+	
+	not UI6(Erro_inv, ErroMedida);
+	
+	and UI8(Aspersao, Usolo, Uar);
+	and UI10(Aspersor, Aspersao, Erro_inv);
+	// SAIDA: LED  7
+	//=================================================
+	
+	// Alarme
+	//=================================================
+		// Como ja tem o erro de nivel, caso o sensor Low esteja desativado, significa que a caixa ta no nivel critico
+	or UI12(Alarme, Low_inv, ErroMedida);
+	// SAIDA: LED RGB
+	//=================================================
+	
+endmodule 
